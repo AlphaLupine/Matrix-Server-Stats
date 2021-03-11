58ab3467ce5379ffca5473c3d91b2d4ea728c37e
Matthew <matthewduff0912@gmail.com>
2021-03-11 01:11:59 +0000

    🚧 Implemented getter for ServerStatus

--- src/lib/wrappers/FivemWrapper.ts
+++ src/lib/wrappers/FivemWrapper.ts
@@ -1,12 +1,41 @@
 import axios from "axios";
 
-const DEFAULT_OPTIONS = {
-    timeout: 1000
-};
-
 export class Server {
-    constructor(ip, options) {
-        if (!ip) throw Error('Missing fivem server IP and/or PORT (IP:PORT)')
+    public ip: string;
+    public timeout: number;
+    constructor(ip: string) {
+        if (!ip) throw Error('Missing fivem server IP and/or PORT (IP:PORT)');
+        this.ip = ip;
+        this.timeout = 1000
+    }
+
+    public async getServerStatus() {
+        return new Promise((send, err) => {
+            axios
+                .get(`http://${this.ip}/players.json`, { timeout: this.timeout })
+                .then(body => {
+                    let status = {
+                        online: true,
+                    }
+                    send(status);
+                }).catch(err => {
+                    let status = {
+                        online: false,
+                        url: err.config.url,
+                        method: err.config.method
+                    }
+                    if (err.response == undefined) send(status);
+                });
+        })
+    }
+
+    public async getPlayers() {
+        await axios
+            .get(`http://${this.ip}/players.json`, { timeout: this.timeout})
+            .then((body) => {
+                let players = body.data;
+                return players.length;
+            });
     }
 
 }
